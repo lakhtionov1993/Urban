507019716eb82880247a68434f3c4311750e2226
lakhtionov1993 <lakhtionov1993@gmail.com>
2024-09-27 22:14:11 +0300

    Update module_1_6.py

--- module_1_6.py
+++ module_1_6.py
@@ -1,5 +1,27 @@
-#Работа со словарями:
-print("Работа со словарями: ")
+#Работа со словарями(версия 1):
+my_dict = {'Vasya': 1975, 'Egor': 1999, 'Masha': 2002}
+print('Dict:', my_dict)
+print('Existing value:' , my_dict.get('Masha'))
+print('Not existing value:' , my_dict.get('Vitalii'))
+my_dict["Artem"] = 1915
+my_dict["Kamila"] = 1981
+print('Deleted value: ' , my_dict.pop('Egor'))
+print('Modified dictionary: ', my_dict, '''
+
+''')
+#Работа с множествами:
+print("Работа с множествам: ")
+my_set = [1, 1,  'Яблоко', 42.314]
+my_set=set(my_set)
+print("Set: " , my_set)
+my_set.add(13)
+my_set.add((5, 6, 1.6))
+my_set.discard(1)
+print("Modified set: " , my_set, '''
+
+''')
+#Работа со словарями(версия 2):
+print("Работа со словарями(версия 2): ")
 my_dict = {"Виталий" : 1993 , "Надежда" : 1993 , "Василиса" : 2019 }
 print(my_dict,)
 print("Введите имя, чтобы узнать год рождения :")
@@ -14,17 +36,4 @@ Name=input()
 a = my_dict.pop(Name, "Человека с таким именем нет в списке!")
 print(a)
 print('''Окончательный список : 
-''',  my_dict, '''
-
-''')
-
-#Работа с множествами:
-print("Работа с множествам: ")
-my_set = [1, 1, 2, 3, 5, 3.14, 3.14,  'Абрикос']
-my_set=set(my_set)
-print(my_set)
-my_set.add(7)
-my_set.add(96)
-print(my_set)
-my_set.discard('Абрикос')
-print(my_set)
+''',  my_dict)
