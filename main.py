209388e4f455e201c4a3f4c508173d33c6da4016
lakhtionov1993 <lakhtionov1993@gmail.com>
2024-09-27 14:57:41 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,16 @@
+#"1st program"
+print(9**0.5*5)
+#"2nd program"
+print(9.99>9.98 and 1000!=1000.1)
+#"3rd program"
+a = 2*2+2
+b = 2*(2+2)
+print(a)
+print(b)
+print(a==b)
+#"4th program"
+string = '123.456'
+string_float=float(string)
+string_1 = string_float * 10 % 10
+string_int = int(string_1)
+print(string_int)
